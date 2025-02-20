04816e1af37e95d014c1a7a8177d63878a849a0d
Kübra Nur Aktaş <kubranurokciyanli@gmail.com>
2025-02-20 14:41:05 +0300

    revised

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,8 @@
 function getElementWidth(content, padding, border) {
-    return parseFloat(content + padding*2 + border*2);
+  let contentWidth = parseFloat(content);
+  let paddingWidth = parseFloat(padding) * 2;
+  let borderWidth = parseFloat(border) * 2;
+  return contentWidth + paddingWidth + borderWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
